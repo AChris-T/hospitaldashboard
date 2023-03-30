404808417f6538a04be28ca3a43981dd03014c3e
Akinfenwa Taiwo Christopher <taiwoakinfenwa2019@gmail.com>
2023-03-30 17:42:12 +0100

    initial commit

---
+++ src/App.js
@@ -0,0 +1,29 @@
+import { Route, Routes } from 'react-router-dom';
+import './App.css';
+import Home from "./pages/home/Home";
+import Login from './pages/Login'
+import User from './scenes/user/User';
+import Dashboard from './scenes/dashboard/Dashboard';
+import Index from './scenes/products/Index';
+import Blog from './scenes/blog/Blog';
+import NotFound from './pages/notfound/NotFound';
+
+function App() {
+  return (
+    <div className='app'> 
+      <Routes>
+          <Route path="/" element={<Home/>}>
+            <Route path ="/user" element={<User/>}/>
+            <Route path ="/dashboard" element={<Dashboard/>}/>
+            <Route path ="/product" element={<Index/>}/>
+            <Route path='/blog' element={<Blog/>}/>
+            </Route>
+            <Route path='login' element={<Login/>}/>
+            <Route path='/error' element={<NotFound/>}></Route>
+
+      </Routes>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,85 @@
+import React from 'react'
+import './navbar.css'
+import SearchIcon from '@mui/icons-material/Search';
+import MenuOpenIcon from '@mui/icons-material/MenuOpen';
+import Box from '@mui/material/Box';
+import Typography from '@mui/material/Typography';
+import Modal from '@mui/material/Modal';
+import Sidebar from './Sidebar';
+import {useState} from 'react';
+import Badge from '@mui/material/Badge';
+import NotificationsIcon from '@mui/icons-material/Notifications';
+import US from "../assests/usa.png"
+import profile from "../assests/joy.png"
+
+
+const style = {
+  position: 'absolute',
+  top: '0%',
+  left: '0%', 
+  width: 300,
+  bgcolor: 'background.paper',
+  border: '2px solid #ffff',
+  boxShadow: 2,
+  scrollY:'scroll',
+};
+
+const Navbar = () => {
+  const [isShown, setIsShown] = useState(false);
+  const [open, setOpen] = React.useState(false);
+  const handleOpen = () => setOpen(true);
+  const handleClose = () => setOpen(false);
+
+  const handleClick = event => {
+    setIsShown(current => !current);
+  }
+
+  return (
+    <div className='navbar'>
+      <div className="navcontainer">
+          <div className="top">
+          <div className="searchItems">
+              {isShown && (
+                <div className='searchItem'>
+                    <div className="searchField">
+                      <div className="inputs">
+                          <SearchIcon className='inputicon'/>
+                          <input type="text" placeholder="Search ...." />
+                       </div>
+                       <a href="/" className='searchbtn'>Search</a>
+                    </div>
+                </div>
+              )} 
+              <div className='searchdetails'>
+              <div className="searchdetail">
+                  <div className='searchIcondetails'>
+                    <SearchIcon className='searchicon' onClick={handleClick}/>
+                  </div>
+                  <div className="searchdetailinfo"> 
+                    <img src={US} alt="us" className='usImage'/>
+                    <Badge badgeContent={4} color="secondary" style={{cursor:'pointer', marginRight:'20px'}}>
+                    <NotificationsIcon color="action"  />
+                    </Badge>
+                    <img src={profile} alt="profile" className='profileImage' />
+                  </div>
+              </div> 
+              </div>
+           </div>
+            <div onClick={handleOpen} className='navbtn'><MenuOpenIcon className='navbarIcon'/></div>
+            <Modal
+             open={open}
+             onClose={handleClose}
+            >
+            <Box sx={style} onClick={handleClose}>
+              <Typography>
+                <Sidebar/>
+              </Typography>
+            </Box>
+            </Modal>
+      </div>
+    </div>
+    </div>
+  )
+}
+
+export default Navbar
